c74b85cc4c817818eb1c8770140e4d387b818596
Shilp <shilppatel77@gmail.com>
2022-05-24 00:15:30 +0530

    filebasedrouting

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is my about page</div>
+  )
+}
+
+export default about
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>This is my contact page</div>
+  )
+}
+
+export default contact
